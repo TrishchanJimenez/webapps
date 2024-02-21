a3a7ca70e4c7185efbe1eeac32db11e4fe255888
TrishchanJimenez <trishchanjimenez06@gmail.com>
2024-02-21 09:41:27 +0800

    Added README

---
+++ README.md
@@ -0,0 +1,36 @@
+# ACTIVITY
+1. Activity p.27-28
+* activity1.php
+2. Activity p.49-50
+* sample.php
+* handle_form.php
+3. Activity p.75
+* activity2.php
+4. Activity p.93
+* activity3.php
+5. Activity p.113
+* courseform.php
+* course.php
+
+# HANDS ON
+1. Hands On p.31
+* handson1.php
+2. Hands On p.53
+* handson2.php
+3. Hands On p.79
+* handson3.php
+4. Hands On p.97
+* handson4.php
+* rectarea.php
+* circarea.php
+5. Hands On p.117
+
+
+# ASSIGNMENT
+1. Assignment p.55
+* assignment1_form.php
+* assignment1_output.php
+2. Assignment p.99
+* assignment2_form.php
+* assignment2_output.php
+3. Assigment p.119-120
--- activity2.php
+++ activity2.php
@@ -1,15 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ACTIVITY</title>
-</head>
-<body>
-    <form action="output.php" method="POST">
-        <b>Enter Length in Centimetre:</b>
-        <input type="text" name="cm" id="" size="20">
-        <input type="submit" value="Convert to Metre and Kilometre" name="submit">
-    </form>
-</body>
-</html>
+<?php
+    $str = "My name is RAFFY";
+    $str5 = "MY name is Jomariss Jungkook";
+    $str = strtolower($str);
+    $str2 = strtoupper($str);
+    $str3 = ucfirst($str);
+    $str4 = lcfirst($str);
+    $str6 = ucwords($str);
+    $str7 = strrev($str);
+    $str8 = strlen($str);
+
+    echo "strtolower: ".$str."<br>";
+    echo "strtoupper: ".$str2."<br>";
+    echo "ucfirst: ".$str3."<br>";
+    echo "lcfirst: ".$str4."<br>";
+    echo "ucwords: ".$str6."<br>";
+    echo "strrev: ".$str7."<br>";
+    echo "strlen: ".$str8."<br>";
+?>
